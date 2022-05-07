4154c51dfbf075a7d967f6bf71b5fa18ec1196ac
Respah-Nafula <nafularespah@gmail.com>
2022-05-07 18:02:48 +0300

    changes

--- app/src/main/java/com/example/halloworld/MainActivity.kt
+++ app/src/main/java/com/example/halloworld/MainActivity.kt
@@ -7,12 +7,18 @@ import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     lateinit var btnBmi:Button
+    lateinit var  btnExchange:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btnBmi = findViewById(R.id.btnBmi)
         btnBmi.setOnClickListener {
             val intent = Intent(this,BMIculculatorActvity::class.java)
+            startActivity(intent)
+        }
+        btnExchange=findViewById(R.id.btnExchange)
+        btnExchange.setOnClickListener {
+            val intent=Intent(this,sendMoney::class.java)
             startActivity(intent)
         }
     }
---
+++ app/src/main/java/com/example/halloworld/sendMoney.kt
@@ -0,0 +1,14 @@
+package com.example.halloworld
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class sendMoney : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_send_money)
+
+    }
+}
